8e66ce26c47e175bda00af4433fdf2f061801f9b
slotocki <sylwester@lotocki.eu>
2026-04-15 18:46:49 +0200

    delete logger

--- libraries/logger.py
+++
@@ -1,7 +0,0 @@
-from robot.api import logger
-
-def loguj_status_symulatora(status_code):
-    if status_code == 200:
-        logger.info(f"Test passed: {status_code}")
-    else:
-        logger.warn(f"Test failed: {status_code}")
